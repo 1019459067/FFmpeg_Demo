3f1f1b8437eeafc788d6811c7c72e4a5d1eee5ef
xiaoweihua <xiaoweihua@sensetime.com>
2016-05-11 16:42:13 +0800

    FFmpeg_Demo: init.

---
+++ FFmpeg_Demo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FFmpeg_Demo
+//
+//  Created by ST on 16/5/11.
+//  Copyright © 2016年 XWH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
